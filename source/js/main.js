ad3786abd1b71c1ad3607e2b12cb770e9bbf30e6
Artem Kapitanov <avkapitanov@yandex.ru>
2020-05-02 19:46:34 +0300

    Add styleguide effects for some blocks

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
   ymaps.ready(function () {
     if (document.querySelector(".contacts__map")) {
       var myMap = new ymaps.Map("map", {
@@ -25,26 +25,33 @@ document.addEventListener("DOMContentLoaded", function(){
   let toggleBtn = document.querySelector(".main-header__toggle");
   let header = document.querySelector(".main-header");
 
-  header.classList.remove("main-header--nojs");
+  if (header) {
+    header.classList.remove("main-header--nojs");
+  }
 
-  toggleBtn.addEventListener("click", function () {
-    header.classList.toggle("main-header--opened");
-  });
-
+  if (toggleBtn) {
+    toggleBtn.addEventListener("click", function () {
+      header.classList.toggle("main-header--opened");
+    });
+  }
 
   let formLink = document.querySelector(".week-product__buy-btn");
   let popup = document.querySelector(".modal");
   let overlay = document.querySelector(".modal__overlay");
 
-  formLink.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.add("modal--opened");
-  });
+  if (formLink) {
+    formLink.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popup.classList.add("modal--opened");
+    });
+  }
 
-  overlay.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("modal--opened");
-  });
+  if (overlay) {
+    overlay.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popup.classList.remove("modal--opened");
+    });
+  }
 
   window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
