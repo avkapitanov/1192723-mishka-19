3a13042669d0aa89ca2cc858df917f9ea06afc94
Artem Kapitanov <avkapitanov@yandex.ru>
2020-05-05 19:51:26 +0300

    Add refresh after js changes. Add youtube video to catalog page

--- source/js/main.js
+++ source/js/main.js
@@ -83,4 +83,19 @@ document.addEventListener("DOMContentLoaded", function () {
       popup.classList.add("modal--opened");
     })
   });
+
+  var videoBlock = document.querySelectorAll( ".manufacture-block__video-wrapper" );
+
+  for (var i = 0; i < videoBlock.length; i++) {
+    videoBlock[i].addEventListener( "click", function() {
+      var iframe = document.createElement( "iframe" );
+
+      iframe.setAttribute( "frameborder", "0" );
+      iframe.setAttribute( "allowfullscreen", "" );
+      iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+
+      this.innerHTML = "";
+      this.appendChild( iframe );
+    } );
+  };
 });
